d61ec12f82493dcb7884040175659fc409aeca9a
bigbro0991 <bigbro0991@gmail.com>
2020-06-17 22:47:58 -0400

    add db

--- project/settings.py
+++ project/settings.py
@@ -57,11 +57,19 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.temphospital
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+                
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
